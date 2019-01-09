448063f263d2a382f6f2b1cfa67b48af6dcf333c
Allison Howard <alliemhow@gmail.com>
2019-01-09 10:51:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,13 +36,15 @@ class Student
         INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
-      
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+
     end
   end
 
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(self.name, self.grade, self.id)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
