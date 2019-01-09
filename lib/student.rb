dd3767c9f474b29bbc9b849f8723d398b0afb8a0
Allison Howard <alliemhow@gmail.com>
2019-01-09 11:06:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,10 +47,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new(@id, @name, @grade)
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row [2]
+    student = self.new(id=nil, name, grade)
   end
 
   def update
